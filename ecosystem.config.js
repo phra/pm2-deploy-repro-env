264e58627c90100728e1fa1775f2615a853a5f8d
phra <greensoncio@gmail.com>
2017-11-20 22:06:45 +0100

    chore: INIT

---
+++ ecosystem.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'API',
+      script    : 'dist/index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production',
+	PORT: 3000
+      },
+      env_dev : {
+        VAR: 'VAR',
+	PORT: 4000
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'phra',
+      host : 'localhost',
+      ref  : 'origin/master',
+      repo : 'git@github.com:phra/pm2-deploy-repro-env.git',
+      path : '/tmp/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'phra',
+      host : 'localhost',
+      ref  : 'origin/master',
+      repo : 'git@github.com:phra/pm2-deploy-repro-env.git',
+      path : '/tmp/dev',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
